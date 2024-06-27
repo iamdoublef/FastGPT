9ece62aa23c58ea1b11d1e92bb38b58dc93004b2
zhengyh <zhengyh@zhongtianinfo.com>
2024-06-27 15:51:56 +0800

    增加bi对接

--- projects/app/src/web/common/hooks/useCopyData.tsx
+++ projects/app/src/web/common/hooks/useCopyData.tsx
@@ -14,6 +14,17 @@ export const useCopyData = () => {
       try {
         if (navigator.clipboard) {
           await navigator.clipboard.writeText(data);
+          //这里是新加的，用来bi界面取值用
+          var oldInput = document.querySelector('input.textConent');  
+          if (oldInput) {  
+              oldInput.remove();  
+          }  
+          // 创建一个新的 input 元素  
+          var newInput = document.createElement('input');  
+          newInput.type = 'text'; // 默认为 'text'，但明确指定总是好的  
+          newInput.className = 'textConent'; // 设置类名  
+          newInput.value = data; // 设置值  
+          document.body.appendChild(newInput);
         } else {
           throw new Error('');
         }
@@ -26,6 +37,18 @@ export const useCopyData = () => {
         textarea.select();
         document.execCommand('copy');
         document.body?.removeChild(textarea);
+        //新加的两种复制方式
+         //这里是新加的，用来bi界面取值用
+         var oldInput = document.querySelector('input.textConent');  
+         if (oldInput) {  
+             oldInput.remove();  
+         }  
+         // 创建一个新的 input 元素  
+         var newInput = document.createElement('input');  
+         newInput.type = 'text'; // 默认为 'text'，但明确指定总是好的  
+         newInput.className = 'textConent'; // 设置类名  
+         newInput.value = data; // 设置值  
+         document.body.appendChild(newInput);
       }
 
       if (title) {
